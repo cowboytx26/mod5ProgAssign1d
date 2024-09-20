29545eabca64fcb2b6211c0e068232761143e699
Brian W Wiatrek <brianwiatrek@aol.com>
2024-09-20 17:06:25 -0500

    Initial code

--- src/ShowImage.java
+++ src/ShowImage.java
@@ -1,34 +1,52 @@
+/*
+Short Description:  This program will display images of four flags on a pane with two flags on each row
+Author:  Brian Wiatrek
+Date:  September 20, 2024
+*/
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class ShowImage extends Application {
     @Override // Override the start method in the Application class
-    public void start(Stage primaryStage) {
-        // Create a pane to hold the image views
-        Pane pane = new HBox(10);
-        pane.setPadding(new Insets(5, 5, 5, 5));
-        Image image = new Image("us.gif");
-        pane.getChildren().add(new ImageView(image));
+    public void start(Stage primaryStage) throws FileNotFoundException {
 
-        ImageView imageView2 = new ImageView(image);
-        imageView2.setFitHeight(100);
-        imageView2.setFitWidth(100);
-        pane.getChildren().add(imageView2);
+        // Read the flag images from file input streams
+        FileInputStream imageInput1 = new FileInputStream("/Users/brianwiatrek/IdeaProjects/mod5ProgAssign1d/flag1.gif");
+        FileInputStream imageInput2 = new FileInputStream("/Users/brianwiatrek/IdeaProjects/mod5ProgAssign1d/flag2.gif");
+        FileInputStream imageInput3 = new FileInputStream("/Users/brianwiatrek/IdeaProjects/mod5ProgAssign1d/flag6.gif");
+        FileInputStream imageInput4 = new FileInputStream("/Users/brianwiatrek/IdeaProjects/mod5ProgAssign1d/flag7.gif");
 
-        ImageView imageView3 = new ImageView(image);
-        imageView3.setRotate(90);
-        pane.getChildren().add(imageView3);
+        //Create a new GridPane
+        GridPane pane = new GridPane();
+
+        //Create new images for each of the files
+        Image image1 = new Image(imageInput1);
+        Image image2 = new Image(imageInput2);
+        Image image3 = new Image(imageInput3);
+        Image image4 = new Image(imageInput4);
+
+        //place the nodes as required on the pane
+        pane.add(new ImageView(image1),0,0);
+        pane.add(new ImageView(image2),0,1);
+        pane.add(new ImageView(image3),1,0);
+        pane.add(new ImageView(image4),1,1);
 
         // Create a scene and place it in the stage
         Scene scene = new Scene(pane);
         primaryStage.setTitle("ShowImage"); // Set the stage title
         primaryStage.setScene(scene); // Place the scene in the stage
         primaryStage.show(); // Display the stage
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
